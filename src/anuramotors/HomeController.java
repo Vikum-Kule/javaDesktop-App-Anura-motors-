45ee2e0e4cc6c467e32bb4e0052e6033b1623fcc
Vikum-Kule <vikumkulatunga@gmail.com>
2021-04-27 14:11:45 -0700

    button navigation added

--- src/anuramotors/HomeController.java
+++ src/anuramotors/HomeController.java
@@ -5,12 +5,17 @@
  */
 package anuramotors;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
 /**
@@ -22,6 +27,20 @@ public class HomeController implements Initializable {
 
     @FXML
     private StackPane contentArea;
+    @FXML
+    private Button btnHome;
+    @FXML
+    private Button btnOrder;
+    @FXML
+    private Button btnStock;
+    @FXML
+    private Button btnNotification;
+    @FXML
+    private Button btnCustomers;
+    @FXML
+    private Button btnAnalysis;
+    @FXML
+    private Button btnSettings;
 
     /**
      * Initializes the controller class.
@@ -30,14 +49,33 @@ public class HomeController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            // TODO
+            Parent root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
+            contentArea.getChildren().removeAll();
+            contentArea.getChildren().setAll(root);
+        } catch (IOException ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
        
         
     }
 
-public void homepage(){
+    @FXML
+    private void homepage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(root);
+    }
+
+    @FXML
+    private void orders(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("order.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(root);
+    }
+
     
-}    
     
     
     
